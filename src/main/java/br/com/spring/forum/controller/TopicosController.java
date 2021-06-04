645f5893bfa3a48b608f189ade4c515d8134e994
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-06-04 00:03:08 -0300

    Utilizando DTO

--- src/main/java/br/com/spring/forum/controller/TopicosController.java
+++ src/main/java/br/com/spring/forum/controller/TopicosController.java
@@ -3,20 +3,19 @@ package br.com.spring.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.spring.forum.controller.dto.TopicoDto;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topc = new Topico("Dúvida", "Dúvida de Spring", new Curso("Spring", "Programação"));
-		return Arrays.asList(topc,topc,topc);
+	public List<TopicoDto> lista() {
+		Topico topc = new Topico("Dúvida - Mais uma", "Dúvida de Spring", new Curso("Spring", "Programação"));
+		return TopicoDto.converter(Arrays.asList(topc,topc,topc));
 	}
 }
